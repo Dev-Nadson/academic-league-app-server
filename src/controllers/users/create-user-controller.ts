b2de5c0a184eef0d85b6be900d4ead12262fe185
Dev-Nadson <nadson.pros@gmail.com>
2025-11-18 15:39:16 -0300

    Feat: Reply melhorada

--- src/controllers/users/create-user-controller.ts
+++ src/controllers/users/create-user-controller.ts
@@ -4,9 +4,12 @@ import { create_user_repo } from "../../repositories/users/create-user-repo.js";
 
 async function create_user_controller(request: FastifyRequest, reply: FastifyReply) {
     const { name, email, password, birthdate, enrollment_number, institution, period, role, avatar_url, is_active } = user_schema.parse(request.body)
-
     const user = await create_user_repo({ name, email, password, birthdate, enrollment_number, institution, period, role, avatar_url, is_active })
-    return reply.status(201).send(user)
+
+    return reply.status(201).send({
+        message: "Usuário criado com sucesso!",
+        data: user
+    })
 }
 
 export { create_user_controller }
