ee82591dc812acb3d4f376e93775351d9c76b6c5
Dev-Nadson <nadson.pros@gmail.com>
2025-11-18 15:36:40 -0300

    Feat: error handler adicionado

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import fastify, { type FastifyInstance } from "fastify";
 import cors from '@fastify/cors'
+import { error_handler_middleware } from "./middlewares/error-handler-middleware.js";
 import { app_routes } from "./routes/index-routes.js";
 import { env } from "./libs/env.config.js";
 
@@ -8,6 +9,7 @@ function create_app(): FastifyInstance {
     const app = fastify()
 
     app.register(cors, { origin: env.ORIGINS, credentials: true })
+    app.setErrorHandler(error_handler_middleware)
     app.register(app_routes)
 
     return app
