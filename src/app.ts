e967320ff672a64c6b2d5a493c8ea0280d0bffee
Dev-Nadson <nadson.pros@gmail.com>
2025-11-18 14:22:41 -0300

    Feat: cors adicionado

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import fastify, { type FastifyInstance } from "fastify";
+import cors from '@fastify/cors'
+import { app_routes } from "./routes/index-routes.js";
+import { env } from "./libs/env.config.js";
+
 
 function create_app(): FastifyInstance {
     const app = fastify()
+
+    app.register(cors, { origin: env.ORIGINS, credentials: true })
+    app.register(app_routes)
 
     return app
 }
