d1a8bad9895acde890c5fe3dc74390ee3827f99b
Dev-Nadson <nadson.pros@gmail.com>
2025-11-19 09:34:12 -0300

    Feat: create user controller adicionado

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,10 +1,11 @@
 import type { FastifyInstance } from "fastify";
 import { create_user_controller } from "../controllers/users/create-user-controller.js";
+import { user_schema } from "../utils/schemas/user-schema.js";
 
 async function users_routes(app: FastifyInstance) {
     app.get("/", () => { console.log("oi") })
     app.get("/:id", () => { })
-    app.post("/", create_user_controller)
+    app.post("/", { schema: user_schema }, create_user_controller)
     app.put("/:id", () => { })
     app.patch("/:id/status", () => { })
     app.delete("/:id", () => { })
