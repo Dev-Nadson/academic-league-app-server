1c9d3337781b2a8ae653f06f57d61b415354397d
Dev-Nadson <nadson.pros@gmail.com>
2025-11-19 09:35:32 -0300

    Feat: swagger adicionado

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,44 @@
 import fastify, { type FastifyInstance } from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
 import cors from '@fastify/cors'
 import { error_handler_middleware } from "./middlewares/error-handler-middleware.js";
 import { app_routes } from "./routes/index-routes.js";
 import { env } from "./libs/env.config.js";
 
-
 function create_app(): FastifyInstance {
     const app = fastify()
 
+    app.register(fastifySwagger), {
+        openapi: {
+            openapi: '3.0.0',
+            info: {
+                title: 'Academic League API',
+                description: 'Documentando a API de gerenciamento de liga acadêmica com swagger',
+                version: '1.0.0'
+            },
+            servers: [
+                {
+                    url: 'http://localhost:4000',
+                }
+            ],
+            tags: [
+                { name: 'user', description: 'User related end-points' },
+                { name: 'code', description: 'Code related end-points' }
+            ],
+            components: {
+                securitySchemes: {
+                    apiKey: {
+                        type: 'apiKey',
+                        name: 'apiKey',
+                        in: 'header'
+                    }
+                }
+            },
+        }
+    }
+
+    app.register(fastifySwaggerUI, { routePrefix: "/docs" })
     app.register(cors, { origin: env.ORIGINS, credentials: true })
     app.setErrorHandler(error_handler_middleware)
     app.register(app_routes)
--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,5 @@ const app = create_app()
 app.listen({ port: env.PORT, host: env.HOST }).then(() => {
     console.log(`Server is running on port: ${env.PORT}`)
     console.log(`Acess on url: http://localhost:${env.PORT}`)
+    console.log(`docs on url: http://localhost:${env.PORT}/docs`)
 })
