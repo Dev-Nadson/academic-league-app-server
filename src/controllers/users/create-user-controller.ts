355d05cc4145cf14a51cbd08ba4274d1e6123663
Dev-Nadson <nadson.pros@gmail.com>
2025-11-23 18:05:38 -0300

    Fix: identação corrigida

--- src/controllers/users/create-user-controller.ts
+++ src/controllers/users/create-user-controller.ts
@@ -4,6 +4,7 @@ import { create_user_repo } from "../../repositories/users/create-user-repo.js";
 
 async function create_user_controller(request: FastifyRequest, reply: FastifyReply) {
     const { name, email, password, birthdate, enrollment_number, institution, period, role, avatar_url, is_active } = user_schema.parse(request.body)
+
     const user = await create_user_repo({ name, email, password, birthdate, enrollment_number, institution, period, role, avatar_url, is_active })
 
     return reply.status(201).send({
