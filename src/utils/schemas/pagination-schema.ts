f8d4c74110477dd13ee55eabee86acbbfe068b82
Dev-Nadson <nadson.pros@gmail.com>
2025-11-23 22:19:48 -0300

    Feat: term adicionado

--- src/utils/schemas/pagination-schema.ts
+++ src/utils/schemas/pagination-schema.ts
@@ -2,7 +2,8 @@ import z from "zod";
 
 const pagination_schema = z.object({
     page: z.coerce.number().positive().default(1),
-    limit: z.coerce.number().default(10)
+    limit: z.coerce.number().default(10),
+    term: z.string().optional()
 })
 
 export { pagination_schema }
