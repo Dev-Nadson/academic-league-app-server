e2fe3f635ce6fb8e5d7af3f337b7b11cbe8dc28b
Dev-Nadson <nadson.pros@gmail.com>
2025-11-23 22:18:20 -0300

    Fix: resposta corrigida

--- src/controllers/users/list-users-controller.ts
+++ src/controllers/users/list-users-controller.ts
@@ -3,13 +3,10 @@ import { pagination_schema } from "../../utils/schemas/pagination-schema.js";
 import { list_users_repo } from "../../repositories/users/list-users-repo.js";
 
 async function list_users_controller(request: FastifyRequest, reply: FastifyReply) {
-    const { page, limit } = pagination_schema.parse(request)
+    const { page, limit } = pagination_schema.parse(request.query)
 
     const users = await list_users_repo({ page, limit })
-    return reply.status(200).send({
-        message: "Todos os usuários",
-        data: users
-    })
+    return reply.status(200).send(users)
 }
 
 export { list_users_controller }
