da199f89bc1ffca5892dfda044f94a49fbe13cbd
Dev-Nadson <nadson.pros@gmail.com>
2025-11-23 22:19:27 -0300

    Feat: função de paginação adicionada

--- src/repositories/users/list-users-repo.ts
+++ src/repositories/users/list-users-repo.ts
@@ -1,12 +1,11 @@
 import type z from "zod";
-import { Knex } from "../../database/config.js";
 import { pagination_schema } from "../../utils/schemas/pagination-schema.js";
+import { pagination } from "../../libs/pagination.js";
 
 type PaginationInput = z.infer<typeof pagination_schema>
 
-async function list_users_repo({ page, limit }: PaginationInput) {
-    const users = await Knex("users").select("*")
-
+async function list_users_repo(pages: PaginationInput) {
+    const users = await pagination("users as u", pages, ["u.id", "u.name", "u.email", "u.birthdate", "u.enrollment_number", "u.institution", "u.period", "u.role", "u.avatar_url", "u.is_active"])
     return users
 }
 
